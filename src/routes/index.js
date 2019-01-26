bbadcbb5383357968151cb82d4b51be05413430c
Alexandr Pogorelov <pogman@MacBook-Pro-Alexandr.local>
2019-01-26 11:45:20 +0300

    add more pages

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,4 +12,24 @@ router.get('/', async ctx => {
 	})
 });
 
+router.get('/about', async ctx => {
+	return ctx.render('about/index', {
+		title: 'Scrappani',
+		phone: '+7-960-477-789-4',
+		email: 'scrappani@yandex.ru',
+	})
+});
+
+router.get('/works', async ctx => {
+	return ctx.render('works/index')
+});
+
+router.get('/contacts', async ctx => {
+	return ctx.render('contacts/index')
+});
+
+router.get('*', async ctx => {
+	return ctx.render('error/index')
+});
+
 module.exports = router;
